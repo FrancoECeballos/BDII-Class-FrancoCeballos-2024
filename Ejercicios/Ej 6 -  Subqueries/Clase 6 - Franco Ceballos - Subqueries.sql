7bb6a130535119ca59a1bfb6f6cc9d096ea7b18f
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-04-08 15:38:28 -0300

    Clase 6 Completada

--- Ejercicios/Ej 6 -  Subqueries/Clase 6 - Franco Ceballos - Subqueries.sql
+++ Ejercicios/Ej 6 -  Subqueries/Clase 6 - Franco Ceballos - Subqueries.sql
@@ -18,3 +18,59 @@ WHERE a.actor_id NOT IN
     FROM film_actor fa);
     
 -- Query 3
+SELECT c.first_name AS 'Nombre', c.last_name AS 'Apellido', COUNT(r.customer_id) AS 'Compras'
+FROM customer c
+INNER JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY r.customer_id
+HAVING COUNT(r.customer_id) = 1;
+
+-- Query 4
+SELECT c.first_name AS 'Nombre', c.last_name AS 'Apellido', COUNT(r.customer_id) AS 'Compras'
+FROM customer c
+INNER JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY r.customer_id
+HAVING COUNT(r.customer_id) >= 2;
+
+-- Query 5
+SELECT a.first_name AS 'Nombre', a.last_name AS 'Apellido', a.actor_id AS 'ID'
+FROM actor a
+WHERE a.actor_id IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%BETRAYED REAR%' OR f.title LIKE '%CATCH AMISTAD%');
+
+-- Query 6
+SELECT a.first_name AS 'Nombre', a.last_name AS 'Apellido'
+FROM actor a
+WHERE a.actor_id IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%BETRAYED REAR%') 
+AND a.actor_id NOT IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%CATCH AMISTAD%');
+
+-- Query 7
+SELECT a.first_name AS 'Nombre', a.last_name AS 'Apellido'
+FROM actor a
+WHERE a.actor_id IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%BETRAYED REAR%') 
+AND a.actor_id IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%CATCH AMISTAD%');
+
+-- Query 8
+SELECT a.first_name AS 'Nombre', a.last_name AS 'Apellido'
+FROM actor a
+WHERE a.actor_id NOT IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%BETRAYED REAR%') 
+AND a.actor_id NOT IN 
+(SELECT fa.actor_id FROM film_actor fa 
+INNER JOIN film f ON fa.film_id = f.film_id 
+WHERE f.title LIKE '%CATCH AMISTAD%');
